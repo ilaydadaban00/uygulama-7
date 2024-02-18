63cdd466dbe095b6e1d21973f726e171ae6ce68a
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-19 02:26:57 +0300

    Add files via upload

---
+++ uygulam 7/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace uygulam_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtMeyve = new System.Windows.Forms.TextBox();
+            this.lbMeyveller = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(90, 152);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtMeyve
+            // 
+            this.txtMeyve.Location = new System.Drawing.Point(90, 105);
+            this.txtMeyve.Name = "txtMeyve";
+            this.txtMeyve.Size = new System.Drawing.Size(100, 20);
+            this.txtMeyve.TabIndex = 1;
+            // 
+            // lbMeyveller
+            // 
+            this.lbMeyveller.FormattingEnabled = true;
+            this.lbMeyveller.Location = new System.Drawing.Point(262, 46);
+            this.lbMeyveller.Name = "lbMeyveller";
+            this.lbMeyveller.Size = new System.Drawing.Size(127, 199);
+            this.lbMeyveller.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(429, 389);
+            this.Controls.Add(this.lbMeyveller);
+            this.Controls.Add(this.txtMeyve);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtMeyve;
+        private System.Windows.Forms.ListBox lbMeyveller;
+    }
+}
+
---
+++ uygulam 7/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulam_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEke_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lbMeyveller.Items.Add(txtMeyve.Text);
+        }
+    }
+}
